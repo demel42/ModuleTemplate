b82488e8cb47fd98823924bcbce7ca6a235dc908
Ch. Damsky <demel41@web.de>
2022-04-20 10:05:51 +0200

    aktualisiert

--- ModuleTemplateDevice/module.php
+++ ModuleTemplateDevice/module.php
@@ -2,12 +2,12 @@
 
 declare(strict_types=1);
 
-require_once __DIR__ . '/../libs/CommonStubs/common.php'; // globale Funktionen
-require_once __DIR__ . '/../libs/local.php';  // lokale Funktionen
+require_once __DIR__ . '/../libs/common.php';
+require_once __DIR__ . '/../libs/local.php';
 
 class ModuleTemplateDevice extends IPSModule
 {
-    use StubsCommonLib;
+    use ModuleTemplate\StubsCommonLib;
     use ModuleTemplateLocalLib;
 
     public function Create()
@@ -15,30 +15,69 @@ class ModuleTemplateDevice extends IPSModule
         parent::Create();
 
         $this->RegisterPropertyBoolean('module_disable', false);
-        
+
+        $this->RegisterPropertyInteger('update_interval', 60);
+
+        $this->RegisterAttributeString('UpdateInfo', '');
+        $this->RegisterAttributeString('external_update_interval', '');
+
         $this->InstallVarProfiles(false);
+
+        $this->RegisterTimer('UpdateStatus', 0, $this->GetModulePrefix() . '_UpdateStatus(' . $this->InstanceID . ');');
+
+        $this->RegisterMessage(0, IPS_KERNELMESSAGE);
     }
 
-    private function CheckConfiguration()
+    public function MessageSink($TimeStamp, $SenderID, $Message, $Data)
     {
-        $s = '';
+        parent::MessageSink($TimeStamp, $SenderID, $Message, $Data);
+
+        if ($Message == IPS_KERNELMESSAGE && $Data[0] == KR_READY) {
+            $this->OverwriteUpdateInterval();
+        }
+    }
+
+    private function CheckModulePrerequisites()
+    {
         $r = [];
 
-        if ($r != []) {
-            $s = $this->Translate('The following points of the configuration are incorrect') . ':' . PHP_EOL;
-            foreach ($r as $p) {
-                $s .= '- ' . $p . PHP_EOL;
-            }
-        }
+        return $r;
+    }
 
-        return $s;
+    private function CheckModuleConfiguration()
+    {
+        $r = [];
+
+        return $r;
+    }
+
+    private function CheckModuleUpdate(array $oldInfo, array $newInfo)
+    {
+        $r = [];
+
+        return $r;
+    }
+
+    private function CompleteModuleUpdate(array $oldInfo, array $newInfo)
+    {
+        return '';
     }
 
     public function ApplyChanges()
     {
         parent::ApplyChanges();
 
-        $vops = 0;
+        if ($this->CheckPrerequisites() != false) {
+            $this->MaintainTimer('UpdateStatus', 0);
+            $this->SetStatus(self::$IS_INVALIDPREREQUISITES);
+            return;
+        }
+
+        if ($this->CheckUpdate() != false) {
+            $this->MaintainTimer('UpdateStatus', 0);
+            $this->SetStatus(self::$IS_UPDATEUNCOMPLETED);
+            return;
+        }
 
         $refs = $this->GetReferenceList();
         foreach ($refs as $ref) {
@@ -52,44 +91,48 @@ class ModuleTemplateDevice extends IPSModule
             }
         }
 
-        $module_disable = $this->ReadPropertyBoolean('module_disable');
-        if ($module_disable) {
-            $this->SetStatus(IS_INACTIVE);
-            return;
-        }
-
         if ($this->CheckConfiguration() != false) {
+            $this->MaintainTimer('UpdateStatus', 0);
             $this->SetStatus(self::$IS_INVALIDCONFIG);
             return;
         }
 
+        $vops = 0;
+
+        $module_disable = $this->ReadPropertyBoolean('module_disable');
+        if ($module_disable) {
+            $this->MaintainTimer('UpdateStatus', 0);
+            $this->SetStatus(self::$IS_DEACTIVATED);
+            return;
+        }
+
         $this->SetStatus(IS_ACTIVE);
+
+        if (IPS_GetKernelRunlevel() == KR_READY) {
+            $this->SetUpdateInterval();
+        }
     }
 
     protected function GetFormElements()
     {
-        $formElements = [];
+        $formElements = $this->GetCommonFormElements('ModulTemplate Device');
 
-        $formElements[] = [
-            'type'    => 'Label',
-            'caption' => 'ModulTemplate Device'
-        ];
-
-        @$s = $this->CheckConfiguration();
-        if ($s != '') {
-            $formElements[] = [
-                'type'    => 'Label',
-                'caption' => $s
-            ];
-            $formElements[] = [
-                'type'    => 'Label',
-            ];
+        if ($this->GetStatus() == self::$IS_UPDATEUNCOMPLETED) {
+            return $formElements;
         }
 
         $formElements[] = [
             'type'    => 'CheckBox',
             'name'    => 'module_disable',
             'caption' => 'Disable instance'
+        ];
+
+        $formElements[] = [
+            'type'    => 'NumberSpinner',
+            'name'    => 'update_interval',
+            'suffix'  => 'Seconds',
+            'minimum' => 0,
+            'caption' => 'Update interval',
         ];
 
         return $formElements;
@@ -99,6 +142,21 @@ class ModuleTemplateDevice extends IPSModule
     {
         $formActions = [];
 
+        if ($this->GetStatus() == self::$IS_UPDATEUNCOMPLETED) {
+            $formActions[] = $this->GetCompleteUpdateFormAction();
+
+            $formActions[] = $this->GetInformationFormAction();
+            $formActions[] = $this->GetReferencesFormAction();
+
+            return $formActions;
+        }
+
+        $formActions[] = [
+            'type'    => 'Button',
+            'caption' => 'Update status',
+            'onClick' => $this->GetModulePrefix() . '_UpdateStatus($id);'
+        ];
+
         $formActions[] = [
             'type'      => 'ExpansionPanel',
             'caption'   => 'Expert area',
@@ -107,7 +165,7 @@ class ModuleTemplateDevice extends IPSModule
                 [
                     'type'    => 'Button',
                     'caption' => 'Re-install variable-profiles',
-                    'onClick' => 'WMS_InstallVarProfiles($id, true);'
+                    'onClick' => $this->GetModulePrefix() . '_InstallVarProfiles($id, true);'
                 ],
             ],
         ];
@@ -123,10 +181,49 @@ class ModuleTemplateDevice extends IPSModule
             ]
         ];
 
-        $formActions[] = $this->GetInformationForm();
-        $formActions[] = $this->GetReferencesForm();
+        $formActions[] = $this->GetInformationFormAction();
+        $formActions[] = $this->GetReferencesFormAction();
 
         return $formActions;
+    }
+
+    private function SetUpdateInterval(int $sec = null)
+    {
+        if (is_null($sec)) {
+            $sec = $this->ReadAttributeString('external_update_interval');
+            if ($sec == '') {
+                $sec = $this->ReadPropertyInteger('update_interval');
+            }
+        }
+        $this->MaintainTimer('UpdateStatus', $sec * 1000);
+    }
+
+    public function OverwriteUpdateInterval(int $sec = null)
+    {
+        if (is_null($sec)) {
+            $this->WriteAttributeString('external_update_interval', '');
+        } else {
+            $this->WriteAttributeString('external_update_interval', $sec);
+        }
+        $this->SetUpdateInterval($sec);
+    }
+
+    public function UpdateStatus()
+    {
+        if ($this->CheckStatus() == self::$STATUS_INVALID) {
+            $this->SendDebug(__FUNCTION__, $this->GetStatusText() . ' => skip', 0);
+            return;
+        }
+
+        /*
+        if ($this->HasActiveParent() == false) {
+            $this->SendDebug(__FUNCTION__, 'has no active parent', 0);
+            $this->LogMessage('has no active parent instance', KL_WARNING);
+            return;
+        }
+         */
+
+        $this->SendDebug(__FUNCTION__, $this->PrintTimer('UpdateStatus'), 0);
     }
 
     public function RequestAction($ident, $value)
